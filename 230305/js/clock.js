bc1b0b75111083710c6ebeafeeb8138f8790b181
DaeHeeAhn <daed5031@gmail.com>
2023-03-05 19:55:14 +0900

    230305 시계 만들기

--- 230305/js/clock.js
+++ 230305/js/clock.js
@@ -1,7 +1,7 @@
 const clock = document.querySelector("h2#clock"); //h2 태그 안의 clock id를 찾아라
 
 function getClock() {
-  const date = new Date(); //date 객체 생성
+  const date = new Date(); //date 객체 생성, Date object는 호출하는 당시의 현재 날짜/시간을 나타냄
   const hours = String(date.getHours()).padStart(2, "0");
   //String()으로 넘버 타입인 date.getHours을 문자열로 바꿔준 후 padStart를 사용해 최소 2글자로 끝내며 공백은 "0"으로 채워줌
   const minutes = String(date.getMinutes()).padStart(2, "0");
@@ -13,4 +13,4 @@ function getClock() {
 // setTimeout(sayHello, 5000); //setTimeout : 무언가를 지정한 ms뒤에 실행(호출되는 함수, ms단위 5초)
 
 getClock(); //getClock 함수 실행, 최초 한 번 실행하지 않으면 1초 뒤에 실행되는 setInterval 때문에 1초 뒤에 보이기 때문
-setInterval(getClock, 1000);
+setInterval(getClock, 1000); //일정 간격으로 함수를 호출하며 ms 단위로 호출되는 시간을 정의
